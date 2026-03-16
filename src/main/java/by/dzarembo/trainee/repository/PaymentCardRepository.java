a9d15a0d6f54653e3c1f954954eceb26109a0ebe
dzarembo <dima.zarembo@gmail.com>
2026-03-16 11:38:00 +0300

    Prevent N+1 queries when loading payment cards

--- src/main/java/by/dzarembo/trainee/repository/PaymentCardRepository.java
+++ src/main/java/by/dzarembo/trainee/repository/PaymentCardRepository.java
@@ -1,6 +1,10 @@
 package by.dzarembo.trainee.repository;
 
 import by.dzarembo.trainee.entity.PaymentCardEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +14,12 @@ import java.util.List;
 
 public interface PaymentCardRepository extends JpaRepository<PaymentCardEntity, Long>,
         JpaSpecificationExecutor<PaymentCardEntity> {
+
+    @EntityGraph(attributePaths = "user")
     List<PaymentCardEntity> findAllByUserId(Long userId);
+
+    @EntityGraph(attributePaths = "user")
+    Page<PaymentCardEntity> findAll(Specification<PaymentCardEntity> specification, Pageable pageable);
 
     @Query(value = "select count(pce.id) from PaymentCardEntity pce where pce.user.id =:userId")
     long countByUserId(@Param("userId") Long userId);
